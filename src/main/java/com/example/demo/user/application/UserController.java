02855b0acb4eb06d1219a6ee907aad3e9e5ac3b7
AndreaCoa <andrea.coa@utec.edu.pe>
2024-06-01 16:03:09 -0500

    fixes in user

--- src/main/java/com/example/demo/user/application/UserController.java
+++ src/main/java/com/example/demo/user/application/UserController.java
@@ -1,9 +1,12 @@
 package com.example.demo.user.application;
 
+import com.example.demo.lista_reproducción.domain.Playlist;
 import com.example.demo.user.domain.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -14,12 +17,12 @@ public class UserController {
 
     @GetMapping("/{user_id}/playlists")
     public ResponseEntity<List<Playlist>> getPlaylists(@PathVariable Integer userId) {
-        return ResponseEntity.ok(userService.getAllPlaylistsByusuario());
+        return ResponseEntity.ok(userService.getAllPlayListsByUserId(userId));
     }
 
     @PostMapping("{user_id}/playlist")
     public ResponseEntity<Void> addPlaylist(@PathVariable Integer userId, @RequestBody Playlist playlist) {
-        userService.createPlaylist(userId, playlist);
+        userService.createPlayList(userId, playlist);
         return ResponseEntity.ok().build();
     }
 
