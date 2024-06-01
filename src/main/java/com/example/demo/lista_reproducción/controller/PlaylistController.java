6db7ed6c30e00fea1b06ea3b6cc233e53af8ec1e
Camila <111703202+CamCove29@users.noreply.github.com>
2024-06-01 15:43:47 -0500

    camilapart2

---
+++ src/main/java/com/example/demo/lista_reproducción/controller/PlaylistController.java
@@ -0,0 +1,4 @@
+package com.example.demo.lista_reproducción.controller;
+
+public class PlaylistController {
+}
---
+++ src/main/java/com/example/demo/lista_reproducción/domain/Playlist.java
@@ -0,0 +1,26 @@
+package com.example.demo.lista_reproducción.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class Playlist {
+    @Id
+    private int idPlaylist;
+    private String nombre;
+    private Date fechaDeCreacion;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    @OneToMany(mappedBy = "playlist")
+    private List<Cancion> canciones;
+
+
+}
