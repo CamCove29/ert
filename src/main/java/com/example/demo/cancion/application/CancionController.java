122aeb3b3d9fba2efda734a8edeb335c3eed43ec
Elvis Alejandro Lopez Gambini <135379186+ElvisLG06@users.noreply.github.com>
2024-06-01 16:06:54 -0500

    Cancion Ready 3

--- src/main/java/com/example/demo/cancion/application/CancionController.java
+++ src/main/java/com/example/demo/cancion/application/CancionController.java
@@ -1,4 +1,25 @@
 package com.example.demo.cancion.application;
 
+import com.example.demo.cancion.domain.CancionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cancion")
+@RequiredArgsConstructor
 public class CancionController {
+    private final CancionService cancionService;
+
+    @GetMapping
+    public ResponseEntity<List<Cancion>> getCanciones() {
+        return ResponseEntity.ok(cancionService.getCanciones());
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createCancion(@RequestBody CancionCreateDto cancionCreateDto) {
+        cancionService.createCancion(cancionCreateDto);
+        return ResponseEntity.ok("Cancion created.");
 }
